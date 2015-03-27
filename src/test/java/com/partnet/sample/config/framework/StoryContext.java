b20cca44ec29fe8a7327aa39fc2955d24c9838f8
Brent Barker <bbarker@part.net>
2015-03-27 16:21:31 -0600

    Fixed project to make it easier to create archetypes

--- src/test/java/com/partnet/sample/config/framework/StoryContext.java
+++ src/test/java/com/partnet/sample/config/framework/StoryContext.java
@@ -1,14 +1,29 @@
-package seauto.cucumber.config.framework;
+/*
+ * Copyright 2015 Partnet, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.partnet.sample.config.framework;
+
 import javax.inject.Inject;
 
-import seauto.cucumber.page.BingSite;
-import seauto.cucumber.page.BingSiteProvider;
+import com.partnet.sample.page.BingSite;
+import com.partnet.sample.page.BingSiteProvider;
 
 import com.partnet.automation.AbstractStoryContext;
 import com.partnet.automation.page.Page;
 import com.partnet.automation.page.PageProvider;
-
-/* Copyright (c) 2014 Partnet, Inc. Confidential and Proprietary. */
 
 /**
  * @author <a href="mailto:bbarker@part.net">bbarker</a>
